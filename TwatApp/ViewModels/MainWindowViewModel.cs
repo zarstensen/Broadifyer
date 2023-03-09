748b31d4fa90cc1c6e54bea3e1c8e0133643ad84
karstensensensen <simonblsoerensen@gmail.com>
2023-03-09 17:05:17 +0100

    - Fixed version number not working in published single file applications

--- TwatApp/ViewModels/MainWindowViewModel.cs
+++ TwatApp/ViewModels/MainWindowViewModel.cs
@@ -50,8 +50,16 @@ namespace TwatApp.ViewModels
         public TwitchNotify notifier;
         public string VersionNumber { get
             {
+                string location;
+
                 Assembly assembly = Assembly.GetExecutingAssembly();
-                FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+                if (assembly.Location != string.Empty)
+                    location = assembly.Location;
+                else
+                    location = Environment.ProcessPath;
+
+                FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(location);
                 string version = fileVersionInfo.ProductVersion;
 
                 return $"Version: {version}";
@@ -60,6 +68,7 @@ namespace TwatApp.ViewModels
 
         public MainWindowViewModel()
         {
+            Trace.WriteLine(VersionNumber);
             // the TwitchNotify instance is retrieved from the app, as this instance needs to be active, even when this window is closed,
             // so its lifetime is therefore not tied to this windows lifetime.
 
